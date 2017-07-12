5c5f5e45a82d9f660da2a65ba50e896551ff421b
arjun-19922107 <arjun.debnath@quantiphi.com>
2017-07-12 16:54:15 +0530

    month_week_identifier_using_list_and_tuple

---
+++ month_week_identifier.py
@@ -0,0 +1,10 @@
+month=("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
+d=(1,2,3,4,5,6,7,8,9,10,11,12)
+mn=str(raw_input("enter the month"))
+print (month.index(mn)+1)
+week=("Mon","Tue","Wed","Thr","Fri","Sat","Sun")
+wk=str(raw_input("enter the Week"))
+print (week.index(wk)+1)
+
+
+
